45fa923cb2792369644eb8af0ec6f1bfdc6e8caa
jimozonguo <2035663350@qq.com>
2020-03-19 12:14:34 +0800

    第一次提交

---
+++ scripts/test_setting.py
@@ -0,0 +1,13 @@
+class TestSetting:
+
+    def test_login(self):
+        print("test_login")
+        assert 1
+
+    def test_login1(self):
+        print("test_login1")
+        assert 1
+
+    def test_login2(self):
+        print("test_login2")
+        assert 0
